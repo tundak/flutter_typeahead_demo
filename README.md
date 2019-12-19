ee197e58896a5223e4bbba1f4ec58e55702c4b11
tundak <sonu5650@gmail.com>
2019-12-19 17:32:03 +0530

    demo search

---
+++ README.md
@@ -0,0 +1,8 @@
+# flutter_typeahead_search
+
+Flutter demo application for Search using https://pub.dev/packages/flutter_typeahead
+
+## Example
+![](https://github.com/tundak/flutter_typeahead_demo/blob/master/flutter_typeahead.gif)
+
+
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_typeahead_search/material_app.dart';
+import 'package:flutter_typeahead_search/cupertino_app.dart';
+
+void main() => runApp(MyMaterialApp());
+//void main() => runApp(MyCupertinoApp());
